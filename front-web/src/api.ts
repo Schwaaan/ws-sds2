e6b243b155f4672f21e326b242652b66777ca1d4
Schwaaan <jair.schwan9@gmail.com>
2021-01-10 19:14:33 -0300

    finish front web

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { OrderPayload } from "./Orders/types";
 
 
 const API_URL = 'http://localhost:8060/'
@@ -10,4 +11,8 @@ export function fetchProducts() {
 
 export function fetchLocalMapBox(local: string) {
     return axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?access_token=${mapboxToken}`)
+}
+
+export function saveOrdeR(payload : OrderPayload){
+    return axios.post(`${API_URL}orders`, payload)
 }
