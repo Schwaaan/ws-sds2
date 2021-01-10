60c35a3c3ae0ad5206237b2ce2055688dacb05a9
Schwaaan <jair.schwan9@gmail.com>
2021-01-10 19:38:41 -0300

    Add env vars

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -2,11 +2,11 @@ import axios from "axios";
 import { OrderPayload } from "./Orders/types";
 
 
-const API_URL = 'http://localhost:8060/'
+const API_URL = process.env.REACT_APP_API_URL;
 const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
 
 export function fetchProducts() {
-    return axios(`${API_URL}products`)
+    return axios(`${API_URL}/products`)
 }
 
 export function fetchLocalMapBox(local: string) {
