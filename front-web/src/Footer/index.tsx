124309fe7be548957436fe2257a11a62ab330f7f
Schwaaan <jair.schwan9@gmail.com>
2021-01-10 14:38:41 -0300

    add footer

---
+++ front-web/src/Footer/index.tsx
@@ -0,0 +1,27 @@
+
+import { ReactComponent as InstagramIcon } from './instagram.svg';
+import { ReactComponent as LinkedinIcon } from './linkedin.svg';
+import './styles.css';
+import { ReactComponent as YoutubeIcon } from './youtube.svg';
+
+function Footer() {
+    return (
+        <footer className="main-footer">
+            App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
+            <div className="footer-icons">
+                <a href="youtube.com/c/devSuperior" target="_new">
+                    <YoutubeIcon />
+                </a>
+                <a href="linkedin" target="_new">
+                    <LinkedinIcon />
+                </a>
+                <a href="instagram" target="_new">
+                    <InstagramIcon />
+                </a>
+
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
